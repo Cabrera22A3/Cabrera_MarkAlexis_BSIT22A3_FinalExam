b274e6ac07d3b9b825bd74d7c7a062bfc7354d72
Cabrera22A3 <solesourc3@gmail.com>
2024-06-29 06:25:21 +0800

    3 ACT7(Dropdown was added.)

--- app/src/main/java/com/example/cabrera_markalexis_bsit22a3_finalexam/MainActivity.kt
+++ app/src/main/java/com/example/cabrera_markalexis_bsit22a3_finalexam/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.cabrera_markalexis_bsit22a3_finalexam
 
-import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -19,7 +22,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.materialPath
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -30,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,6 +55,48 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.cabrera_markalexis_bsit22a3_finalexam.ui.theme.Cabrera_MarkAlexis_BSIT22A3_FinalExamTheme
+
+public val Icons.Filled.ExpandMore: ImageVector
+    get() {
+        if (_expandMore != null) {
+            return _expandMore!!
+        }
+        _expandMore = materialIcon(name = "Filled.ExpandMore") {
+            materialPath {
+                moveTo(16.59f, 8.59f)
+                lineTo(12.0f, 13.17f)
+                lineTo(7.41f, 8.59f)
+                lineTo(6.0f, 10.0f)
+                lineToRelative(6.0f, 6.0f)
+                lineToRelative(6.0f, -6.0f)
+                close()
+            }
+        }
+        return _expandMore!!
+    }
+
+private var _expandMore: ImageVector? = null
+
+public val Icons.Filled.ExpandLess: ImageVector
+    get() {
+        if (_expandLess != null) {
+            return _expandLess!!
+        }
+        _expandLess = materialIcon(name = "Filled.ExpandLess") {
+            materialPath {
+                moveTo(12.0f, 8.0f)
+                lineToRelative(-6.0f, 6.0f)
+                lineToRelative(1.41f, 1.41f)
+                lineTo(12.0f, 10.83f)
+                lineToRelative(4.59f, 4.58f)
+                lineTo(18.0f, 14.0f)
+                close()
+            }
+        }
+        return _expandLess!!
+    }
+
+private var _expandLess: ImageVector? = null
 
 //same idea sa Activity 8 pero tinanggal ko na po yung title since diko naman siya need
 enum class PKMNScreen{
@@ -186,40 +239,28 @@ fun BodyContentAbout(modifier: Modifier = Modifier, navController: NavController
                     .align(Alignment.Top)
             )
         }
-
-        Row {
-            LazyColumn(modifier = Modifier.fillMaxSize(.8f)){
-                item{
-                    //Description about the game.
-                    Text(
-                        text = "This game was created by the use of Android Studio Application."+
-                                " The things that I used in this code are Text, TextField, Column,"+
-                                " Row, and some modifier to make my UI to be aligned into the center."+
-                                " As the continuous commit in my Github Account, I will update and upgrade"+
-                                " my system's features based on the given activity of my professor. "+
-                                " As for the given activities, the first one was the activities which "+
-                                " includes Activities from 1 to 3 that Manipulation of Text, Images and "+
-                                " Column to design my application. The next Activity was the 4th one which"+
-                                " is the Usage of Random function and Button in an application. The 5th one was"+
-                                " the Interacting with the user and usage of tests in an application. The 6th one"+
-                                " was the Scrollable List with the use of LazyColumn function, which I used here in"+
-                                " the abouts page. The 7th one was the Different Design or Animation you can do with"+
-                                " your application. The 8th one was the NavHost Controller, Linking different kotlin "+
-                                " files which I used in the buttons to navigate the screens for my system. "+
-                                " The 9th one was the Connecting to the internet, usage of Retrofit Services. And"+
-                                " lastly was the 10th one which is the Database connection.",
-                        fontSize = 15.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Justify,
-                        color = Color.White,
-                        modifier = Modifier
-                            .wrapContentSize(Alignment.TopCenter)
-                            .align(Alignment.Top)
-                            .padding(start = 20.dp, end = 20.dp, top = 15.dp)
-                    )
-                }
-            }
+        Spacer(//para siyang margin para sa button
+            modifier = Modifier
+                .height(16.dp)
+        )
+        Row{
+            //ss
+            Item(titleChu = stringResource(R.string.titleDropDownDescription1),
+                descChu = stringResource(R.string.descDropDownDescription1))
         }
+        Spacer(//para siyang margin para sa button
+                modifier = Modifier
+                    .height(16.dp)
+                )
+        Row{
+            //ss
+            Item(titleChu = stringResource(R.string.asdf),
+                descChu = stringResource(R.string.ghjkl))
+        }
+        Spacer(//para siyang margin para sa button
+            modifier = Modifier
+                .height(16.dp)
+        )
         Row {
             //Back Button
             Button(
@@ -240,9 +281,12 @@ fun BodyContentGame(modifier: Modifier = Modifier, navController: NavController)
     var answerKey by remember { mutableStateOf("Press Check Button") }
     var answer by remember { mutableStateOf("") }
     var result by remember { mutableStateOf(1) }
+    var scoreboard by remember { mutableStateOf(0) }
+
     val imageResource = when (result) {
         1 -> R.drawable.pikachu
         2 -> R.drawable.bulbasaur
+        3 -> R.drawable.squirtle
         else -> R.drawable.squirtle
     }
     var pokemonName by remember { mutableStateOf("") }
@@ -250,7 +294,7 @@ fun BodyContentGame(modifier: Modifier = Modifier, navController: NavController)
         pokemonName = stringResource(R.string.pikachu)
     }else if(result == 2){
         pokemonName = stringResource(R.string.bulbasaur)
-    }else {
+    }else if(result == 3){
         pokemonName = stringResource(R.string.squirtle)
     }
 
@@ -258,6 +302,11 @@ fun BodyContentGame(modifier: Modifier = Modifier, navController: NavController)
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Row {
+            Button(onClick = {}){
+                Text("Score: ${scoreboard}")
+            }
+        }
         Row (
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
@@ -295,9 +344,11 @@ fun BodyContentGame(modifier: Modifier = Modifier, navController: NavController)
                 Button(onClick = {
                     if (answer == pokemonName) {
                         answerKey = "CORRECT"
+                        scoreboard += 1
                         // Modifier.background(Color.Green)
                     } else {
                         answerKey = "WRONG"
+                        scoreboard -= 1
                         //Modifier.background(Color.Red)
                     }
                 }
@@ -397,6 +448,85 @@ fun Footer(){
     }
 }
 
+//Dropdown
+
+@Composable
+fun Item(
+    titleChu: String,
+    descChu:  String,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.secondaryContainer
+        else MaterialTheme.colorScheme.primaryContainer,
+    )
+    Card(
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+                .background(color = color)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            ) {
+                Text(
+                    text = titleChu,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .wrapContentSize(Alignment.TopCenter)
+                        .align(Alignment.Top)
+                )
+                Spacer(Modifier.weight(1f))
+                ItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded },
+                )
+            }
+            if (expanded) {
+                LazyColumn(modifier = Modifier.fillMaxSize(.8f)) {
+                    item {
+                        //Description about the game.
+                        Text(
+                            text = descChu,
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Justify,
+                            color = Color.Black,
+                            modifier = Modifier
+                                .wrapContentSize(Alignment.TopCenter)
+                                .padding(start = 20.dp, end = 20.dp, top = 15.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+@Composable
+private fun ItemButton(expanded: Boolean,onClick: () -> Unit,modifier: Modifier = Modifier) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
